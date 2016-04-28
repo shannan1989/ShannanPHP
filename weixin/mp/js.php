d7713d94a07141dd5e50b3a976d84ed746fb6a2b
山南 <zhangpeichaook@163.com>
2016-04-28 15:09:41 +0800

    weixin mobile web js

--- weixin/mp/js.php
+++ weixin/mp/js.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>微信网页js接口</title>
     </head>
     <body>
         <?php
@@ -12,7 +12,7 @@
         <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
         <script>
             $(document).ready(function () {
-               wx.config({
+                wx.config({
                     debug: false,
                     appId: '<?php echo $sign_package['appId']; ?>',
                     timestamp: <?php echo $sign_package['timestamp']; ?>,
